4e7f78c96e8d2d1e0b53cc6f8d7f7ccee641749a
omid <omid.panakari.s@gmail.com>
2023-03-06 15:21:50 +0330

    add register file

---
+++ source/RegisterFile.v
@@ -0,0 +1,33 @@
+module RegisterFile (
+    clk,
+    rst,
+    Rn,
+    Rm,
+    WB_dest,
+    WB_value,
+    WB_en,
+    val_Rn,
+    val_Rm
+);
+    input clk, rst, WB_en;
+    input [3:0] Rn, Rm, WB_dest;
+    input [31:0] WB_value;
+    output [31:0] val_Rn, val_Rm;
+    reg [31:0] registers[15:0];
+    integer i;
+
+
+    assign val_Rn = registers[Rn];
+    assign val_Rm = registers[Rm];
+
+    always @(negedge clk, posedge rst) begin
+        if (WB_en)
+            registers[WB_dest] <= WB_value;
+        if (rst) begin
+            for (i = 0; i < 16; i = i + 1) begin
+                registers[i] <= 32'd0;
+            end
+        end
+    end
+
+endmodule
--- source/TopLevel.v
+++ source/TopLevel.v
@@ -3,11 +3,14 @@ module TopLevel (
     rst
 );
     input clk, rst;
-    wire [31:0] IF_pc, ID_pc, Ex_pc, Mem_pc, instruction, current_pc;
+    wire WB_en;
+    wire [3:0] WB_dest;
+    wire [31:0] IF_pc, ID_pc, Ex_pc, Mem_pc, instruction, current_pc, WB_value, val_Rn, val_Rm;
 
     InstructionFetch IF(clk, rst, 1'b0, 1'b0, 32'd0, IF_pc, current_pc);
     InstructionMemory IM(current_pc, instruction);
     IFReg IF_Reg(clk, rst, IF_pc, ID_pc);
+    RegisterFile RF(clk, rst, instruction[19:16], instruction[15:12], WB_dest, WB_value, WB_en, val_Rn, val_Rm);
     InstructionDecode ID(clk, rst, ID_pc);
     IDReg ID_Reg(clk, rst, ID_pc, Ex_pc);
     Execution Ex(clk, rst, Ex_pc);
