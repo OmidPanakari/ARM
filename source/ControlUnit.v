ffc56b5600fa7150eb1d43b18391a6553740d952
Amirfery <Amirferyg@gmail.com>
2023-04-01 19:37:53 +0330

    fix id

--- source/ControlUnit.v
+++ source/ControlUnit.v
@@ -7,12 +7,12 @@ module ControlUnit (
     input s;
     input [1:0] mode;
     input [3:0] op_code;
-
+    output [8:0] controls;
     wire [3:0] alu_command;
-    wire mem_read, mem_write, b, status;
+    wire mem_read, mem_write, b, status, wb_en;
 
     always @(mode, op_code, s) begin
-        {mem_read, mem_write} = 2'd0;
+        {mem_read, mem_write, wb_en} = 3'd1;
         case ({mode,op_code})
             6'b001101:
                 alu_command = 4'b0001;
@@ -36,17 +36,21 @@ module ControlUnit (
                 alu_command = 4'b1000;
             6'b001010:
                 alu_command = 4'b0100;
+                wb_en = 1'b0;
             6'b001000:
                 alu_command = 4'b0110;
+                wb_en = 1'b0;
             6'b010100:
                 alu_command = 4'b0010;
                 mem_read = s;
                 mem_write = ~s;
+                wb_en = ~s;
             default:
                 alu_command = 4'b0000;
         endcase
         b = (mode == 2'b10);
         status = (mode == 2'b00) ? s : 1'b0;
     end
+    assign controls = {wb_en, mem_read, mem_write, alu_command, b, status};
 
 endmodule
--- source/InstructionDecode.v
+++ source/InstructionDecode.v
@@ -1,21 +1,63 @@
 module InstructionDecode (
     clk,
     rst,
+    hazard,
+    wb_dest,
+    wb_value,
+    wb_en,
     condition_check,
-    instruction
+    instruction,
+    pc,
+    two_src,
+    controls_out,
+    pc_out,
+    val_rn,
+    val_rm,
+    imm_out,
+    shift_operand,
+    dest,
+    rn
 );
-    input clk, rst;
-    input [3:0] condition_check;
-    input [31:0] instruction;
-    wire imm, s;
+    input clk, rst, hazard, wb_en;
+    input [3:0] condition_check, wb_dest;
+    input [31:0] instruction, wb_value, pc;
+
+
+    output two_src, imm_out;
+    output [8:0] controls_out;
+    output [31:0] pc_out;
+    output [31:0] val_rn;
+    output [31:0] val_rm;
+    output [11:0] shift_operand;
+    output [23:0] signed_imm;
+    output [3:0] dest, rn;
+
+
+    wire imm, s, condition_check_out, mem_write_en;
     wire[1:0] mode;
-    wire[3:0] cond, op_code, rn, rd;
+    wire[3:0] cond, op_code, rd. rm;
+    wire [8:0] controls;
+
+
     assign imm = instruction[25];
+    assign imm_out = imm;
+    assign signed_imm = instruction[23:0];
+    assign shift_operand = instruction[11:0];
+    assign mem_write_en = controls_out[6]
     assign cond = instruction[31:28];
     assign op_code = instruction[24:21];
     assign s = instruction[20];
     assign mode = instruction[27:26];
     assign rn = instruction[19:16];
     assign rd = instruction[15:12];
+    assign dest = rd;
+    assign rm = instruction[3:0];
+    assign controls_out = ~condition_check_out | hazard ? controls : 9'd0;
+    assign two_src = ~imm | mem_write_en;
+
+
+    ConditionCheck conditionCheck(cond, condition_check, condition_check_out);
+    ControlUnit controlUnit(mode, op_code, s, controls);
+    RegisterFile registerFile(clk, rst, rn, mem_write_en ? rd : rm, wb_dest, wb_value, wb_en, val_rn, val_rm);
 
 endmodule
