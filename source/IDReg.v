2c66b9e95908fa42c4f8840896d584c9fc53cfdf
Amirfery <Amirferyg@gmail.com>
2023-04-01 20:24:05 +0330

    minor bugs fixed

--- source/IDReg.v
+++ source/IDReg.v
@@ -36,13 +36,13 @@ module IDReg (
     input [31:0] pc, val_rm, val_rn;
     input [11:0] shift_operand;
     input [23:0] signed_imm;
-    input wb_en, mem_read, b, status, imm, flush;
+    input wb_en, mem_read, b, status, imm, flush, mem_write;
     input [3:0] alu_command, dest, status_reg_data;
     
     output reg [31:0] pc_out, val_rm_out, val_rn_out;
     output reg [11:0] shift_operand_out;
     output reg [23:0] signed_imm_out;
-    output reg wb_en_out, mem_read_out, b_out, status_out, imm_out;
+    output reg wb_en_out, mem_read_out, b_out, status_out, imm_out, mem_write_out;
     output reg [3:0] alu_command_out, dest_out, status_reg_data_out;
 
     //TODO: add flush
@@ -59,6 +59,7 @@ module IDReg (
         imm_out <= imm;
         alu_command_out <= alu_command;
         dest_out <= dest;
+        mem_write_out <= mem_write;
         status_reg_data_out <= status_reg_data;
         pc_out <= pc;
     end
